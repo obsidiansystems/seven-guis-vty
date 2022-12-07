e4f97ea4533bcab68372dab05393441a393f9f92
Ali Abrar <ali.abrar@obsidian.systems>
2022-12-07 16:20:13 -0400

    shell: use reflex-vty 0.3.1.0

--- default.nix
+++ default.nix
@@ -1,6 +1,15 @@
 let
   reflex-platform = import ./reflex-platform { };
-  callCabal2nix = reflex-platform.ghc.callCabal2nix;
+  ghc = reflex-platform.ghc.override {
+    overrides = self: super: {
+      reflex-vty = self.callHackageDirect {
+        pkg = "reflex-vty";
+        ver = "0.3.1.0";
+        sha256 = "0nm4wiy03b19r3lgv7zzybs1bdlsk9lhfhvd0l7c386lkz9bm2yh";
+      } {};
+    };
+  };
+  callCabal2nix = ghc.callCabal2nix;
   overrideCabal = reflex-platform.overrideCabal;
   # Only build the given executable target
   buildOnlyExecutable = target: drv: overrideCabal drv {
